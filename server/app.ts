40e7b11fa92e22137b373ed82752c3bfea9eb0ed
Grace Tveit <grace.tveit@gmail.com>
2021-09-30 11:32:33 -0700

    feat: Adding express routes for authentication

--- server/app.ts
+++ server/app.ts
@@ -4,10 +4,15 @@ import express from 'express';
 import path from 'path';
 import morgan from 'morgan';
 
+import auth from './auth';
+
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(process.cwd(), 'public')));
+
+app.use('/auth', auth);
 
 app.use('*', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public/index.html'));
--- server/db/auth.ts
+++ server/db/auth.ts
@@ -1,17 +1,8 @@
-import { User } from '.prisma/client';
 import * as argon2 from 'argon2';
 import jwt from 'jsonwebtoken';
 import db from './';
 
-const hashPassword = async (pw: string) => {
-  return await argon2.hash(pw);
-};
-
-const verifyPassword = async (user: User, pw: string) => {
-  return await argon2.verify(user.pwHash, pw);
-};
-
-const generateToken = async (username: string, pw: string) => {
+export const generateToken = async (username: string, pw: string) => {
   const user = await db.user.findUnique({ where: { username } });
   if (!user || (await argon2.verify(user.pwHash, pw))) {
     const error = Error('Incorrect Username/Password');
@@ -20,7 +11,7 @@ const generateToken = async (username: string, pw: string) => {
   return jwt.sign(user.id, process.env.JWT);
 };
 
-const verifyToken = async (token: string) => {
+export const verifyToken = async (token: string) => {
   try {
     const id = jwt.verify(token, process.env.JWT);
     const user = await db.user.findUnique({ where: { id: id as string } });
