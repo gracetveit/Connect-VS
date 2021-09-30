8eed7911cda524b468aa4baf87e5e98f2e122790
Grace Tveit <grace.tveit@gmail.com>
2021-09-30 08:36:58 -0700

    feat: Initialized Redux store

---
+++ client/store/index.ts
@@ -0,0 +1,13 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+const reducer = combineReducers({});
+
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
+
+export default store;
